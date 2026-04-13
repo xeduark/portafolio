be001cf265e8545db895426b930a7a9bf2c790c2
xeduark <jemunozqui@cesde.net>
2026-04-13 13:58:55 -0500

    fix info

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
-
+import path from 'path'
 import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://porfolio.dev/',
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
+  },
 })
